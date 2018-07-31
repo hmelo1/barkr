51490fd696c1a6aca1fadd2cd577f912f7e1fbff
Penguino <heriberto@melo.nyc>
2018-07-31 15:45:43 -0400

    Configuring settings controller

---
+++ www/js/controllers/settings.js
@@ -0,0 +1,30 @@
+'use strict';
+
+app.controller('SettingCtrl', function(Auth){
+    var sett = this;
+
+    sett.maxAge = 25;
+    sett.bigDog = false;
+    sett.smallDog = true;
+    sett.mediumDog = false;
+
+    sett.changeMaxAge = function(){
+        
+    };
+
+    sett.selectBigDog = function(){
+        
+    };
+
+    sett.selectSmallDog = function(){
+        
+    };
+
+    sett.selectMediumDog = function(){
+        
+    };
+
+    sett.logout = function(){
+        
+    };
+})
