e1e49681cc900a5107f129c8ea5ed459b119d7e2
Penguino <heriberto@melo.nyc>
2018-08-01 16:46:38 -0400

    Created messaging modals

--- www/js/controllers/match.js
+++ www/js/controllers/match.js
@@ -4,6 +4,10 @@ app.controller('MatchCtrl', function(Match, Auth, uid, $scope, Like, $ionicModal
     
     var matc = this;
 
+    matc.currentUser = profile;
+    matc.message = '';
+    var viewScroll = $ionicScrollDelegate.$getByHandle('userMessageScroll');
+
     function init(){
         matc.list = [];
 
@@ -36,12 +40,41 @@ app.controller('MatchCtrl', function(Match, Auth, uid, $scope, Like, $ionicModal
         $scope.modal = modal;
     })
 
-    matc.openMessageModal = function(){
-        $scope.modal.show();
-    };
+	matc.openMessageModal = function(matchUid){
+		Auth.getProfile(matchUid).$loaded()
+		.then(function(profile){
+			matc.currentMatchUser = profile;
+			Messages.historyMessages(matchUid, uid).$loaded()
+			.then(function(data){
+				matc.messages = data;
+				$scope.modal.show();
+				$timeout(function() {
+					viewScroll.scrollBottom();
+				}, 0);
+			})
+		})
+	};
+
 
     matc.closeMessageModal = function(){
         $scope.modal.hide();
     };
 
+    matc.sendMessage = function(){
+
+		if(matc.message.length > 0 ){
+				
+				matc.messages.$add({
+					uid: matc.currentUser.$id,
+					body: matc.message,
+					timestamp: firebase.database.ServerValue.TIMESTAMP
+					
+				}).then(function(){
+					matc.message = '';
+					$timeout(function() {
+			          viewScroll.scrollBottom();
+			        }, 0);
+				})			
+		}
+	};
 })
